3dc8a95e62f4c52808fa0a477095d5d05d9dac8b
Hossein Amirinia <amirynia@yahoo.com>
2019-11-13 20:05:57 -0500

    edges

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
-df = pd.read_csv('TreeLevel.csv')
+df = pd.read_csv('parent_child.csv')
 print(df.sort_values("Level",ascending=False))
 
 
@@ -12,17 +12,22 @@ G = nx.Graph()
 for index, row in df.iterrows():
     #print(row['Function'])
     G.add_node(row['Function'],pos=(random.randint(1,1000),row['Level']))
-nx.draw(G, nx.get_node_attributes(G, 'pos'), with_labels=True, node_size=400)
         #ani = animation.FuncAnimation(fig, animate, interval=1000)
 
 
 listf = df['Level']
 print(max(listf))
 for i in range(max(listf)):
-    print("level ",max(listf)-i)
+    print("\n level ",max(listf)-i)
     print(df.loc[df['Level'] == max(listf)-i])
+    #print(row['Parent']=="NaN")
+for index, row in df.iterrows():
+    if( pd.isna(row['Parent']) == False):
+        print(row['Parent'],row['Function'],pd.isna(row['Parent']))
+        G.add_edge(row['Function'],row['Parent'])
 
 #for index,row in df.iterrows():
 #    print(row.Level)
+nx.draw(G, nx.get_node_attributes(G, 'pos'), with_labels=True, node_size=400)
 
 plt.show()
